fc19e4776c2e7f016b81dea67b1a8c16dc288478
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-06-22 14:05:10 -0500

    Se agregaron los metodo y atributos para guardar en el campo de la contraseña/Comentado pero no tan claro
    Lo que se hizo fue crear un atributo password el cual se puede sobreescribir o poner otro valor en ese atributo
    y cuando se sobreescribiese ese atributo lo que haria es poner en el campo de la base de datos el hash de la
    cadena que se ingreso en el atributo y meterle un hash.
    
    En el otro metodo, solo lo que hace es checar si su hash de la palabra que se metio en su parametro es el mismo que se tiene en el
    campo de password_hash y dar un True o un false segun lo que cheque

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -16,6 +16,28 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+    
+    #Se hace un atributo en el cual se guarda la clave en cadena sencilla
+    
+    #Si se intenta leer no se va a poder porque va a dar un error
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    #Entonces cuando se establece o se pone un valor en el atributo, se da la siguiente funcion
+    @password.setter
+    #La funcion con el mismo nombre del metodo, pero con la unica diferencia de que el parametro "password" es un atributo
+    #Entonces cuando se sobreescribe el atributo, este sobreescribe otro atributo que sería el password_hash y lo que mete ahi es la contraseña de hash
+    #que sería el atributo password que se genera un hash
+    def password(self, password):
+        #Guarda en su espacio/variable el hash del atributo password
+        self.password_hash = generate_password_hash(password)
+
+    #Este metodo lo que hace es tomar  la palabra para verificar si es la misma, si son iguales los hash entonces es correcto
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
     def __repr__(self):
         return '<User %r>' % self.username
