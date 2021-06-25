ba8d28a5f5acf0500e4b36a5fa9ad62d889db345
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-06-25 15:08:14 -0500

    Se agrego lo de flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,17 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_login import LoginManager
+
+
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -21,7 +26,14 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
+    #Registro del modelo principal para las rutas principales
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    
+    # Registro del modelo de autenticación
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    login_manager.init_app(app)
 
     return app
