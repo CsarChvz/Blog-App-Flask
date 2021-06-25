c881ca80f94db331ccd94d10b22451e91fc5af19
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-06-25 15:10:18 -0500

    Se agrego lo del flask-login y UserMixin, esto para que los objetos de la aplicación trabaje cerca con el modelo de flask-login

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
+from enum import unique
+from operator import index
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+#Este decorador es usado para registrar la funcion con Flask-Login, el cual va a llamara cuando se necesite devolver informacion acerca del usuario
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -11,12 +20,14 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
     
     #Se hace un atributo en el cual se guarda la clave en cadena sencilla
     
