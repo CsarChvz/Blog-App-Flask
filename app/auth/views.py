33fbbf6f98100fd0201751a0c460ba1662ac9264
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-06-26 17:22:31 -0500

    Cache auth

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,6 +6,7 @@ from wtforms import form
 from . import auth
 from .forms import LoginForm, RegistrationForm
 from ..models import User
+from app import db
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -29,7 +30,15 @@ def logout():
     return redirect(url_for('main.index'))
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,\
+            username=form.username.data,\
+                password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can login now')
+        return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
