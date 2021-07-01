c28027eb33d4a08fd13eb3741cb4b18e3d3073a1
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-07-01 18:30:41 -0500

    Se realizo el formulario para actualizar solo algunos datosparausuarios

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.simple import TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
 class NameForm(FlaskForm):
     name = StringField('Cuál es tu nombre?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class EditProfile(FlaskForm):
+    name = StringField('Nombre Real', validators=[Length(0, 64)])
+    location = StringField('Lugar', validators=[Length(0, 64)])
+    about_me = TextAreaField('Acerca de mi')
+    submite = SubmitField('Enter')
