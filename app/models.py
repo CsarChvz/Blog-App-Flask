b995921a34f5b477f0a8d3357f45643a94704a1f
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-07-02 22:43:01 -0500

    SE DESCONOCE QUE SE CAMBIO

--- app/models.py
+++ app/models.py
@@ -180,7 +180,7 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
-    #timestamp = db.Column(db.DateTime(timezone=True), index=True, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime(timezone=True), index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 @login_manager.user_loader
