c86fc2cbc713a264091b89af4b17313bcb6cf83f
CsarChvz <cesarchavezrodriguez9@gmail.com>
2021-07-06 14:18:21 -0500

    Se coloco un nuevo modelo y nuevos metodo en la parte de Usuarios

--- app/models.py
+++ app/models.py
@@ -18,6 +18,7 @@ import hashlib
 from markdown import markdown
 import bleach
 
+
 class Permission:
 
     FOLLOW = 1
@@ -83,6 +84,12 @@ class Role(db.Model):
     def is_administrator(self):
         return self.can(Permission.ADMIN)
 
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),\
+                            primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),\
+                            primary_key=True)
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -98,6 +105,17 @@ class User(UserMixin, db.Model):
     avatar_hash = db.Column(db.String(32))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    followed = db.relationship('Follow',\
+                                foreign_keys=[Follow.follower_id],
+                                backref=db.backref('follower', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
+
+    followers = db.relationship('Follow',
+                                foreign_keys=[Follow.followed_id],
+                                backref=db.backref('followed', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -163,6 +181,27 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(follower=self, followed=user)
+            db.session.add(f)
+
+    def unfollow(self, user):
+        f = self.followed.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
+
+    def is_following(self, user):
+        if user.id is None:
+            return False
+        return self.followed.filter_by(followed_id=user.id).first() is not None
+
+    def is_followed_by(self, user):
+        if user.id is None:
+            return False
+        return self.followers.filter_by(follower_id=user.id).first() is not None
+        
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -197,3 +236,4 @@ class Post(db.Model):
              strip=True))
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
+
